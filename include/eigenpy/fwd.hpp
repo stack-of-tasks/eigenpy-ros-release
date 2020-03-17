ae028ad172038b885eb397ce2325e3b8fb14b764
Wolfgang Merkt <w.merkt@gmail.com>
2020-03-17 15:23:00 +0000

    Rebase from 'release/melodic/eigenpy'

--- include/eigenpy/fwd.hpp
+++ include/eigenpy/fwd.hpp
@@ -1,32 +1,33 @@
 /*
- * Copyright 2014-2019, CNRS
- * Copyright 2018-2019, INRIA
+ * Copyright 2014-2020 CNRS INRIA
  */
 
 #ifndef __eigenpy_fwd_hpp__
 #define __eigenpy_fwd_hpp__
 
+#include "eigenpy/config.hpp"
+
 #include <boost/python.hpp>
 #include <Eigen/Core>
 
-#include <numpy/numpyconfig.h>
-#ifdef NPY_1_8_API_VERSION
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#endif
+#define NO_IMPORT_ARRAY
+  #include "eigenpy/numpy.hpp"
+#undef NO_IMPORT_ARRAY
 
-#include <numpy/noprefix.h>
-
-#ifdef NPY_ALIGNED
 #if EIGEN_VERSION_AT_LEAST(3,2,90)
   #define EIGENPY_DEFAULT_ALIGNMENT_VALUE Eigen::Aligned16
 #else
   #define EIGENPY_DEFAULT_ALIGNMENT_VALUE Eigen::Aligned
 #endif
-#else
-  #define EIGENPY_DEFAULT_ALIGNMENT_VALUE Eigen::Unaligned
-#endif
+
+#define EIGENPY_NO_ALIGNMENT_VALUE Eigen::Unaligned
 
 #include "eigenpy/expose.hpp"
 
-#endif // ifndef __eigenpy_fwd_hpp__
+namespace eigenpy
+{
+  template<typename MatType> struct EigenToPy;
+  template<typename MatType> struct EigenFromPy;
+}
 
+#endif // ifndef __eigenpy_fwd_hpp__
