e2a709e229a0fa693dd65101383e1e6c64ea480c
Wolfgang Merkt <w.merkt@gmail.com>
2022-05-09 12:19:51 +0100

    Rebase from 'debian/foxy/eigenpy'

--- unittest/python/test_eigen_ref.py
+++ unittest/python/test_eigen_ref.py
@@ -49,7 +49,7 @@ def test(mat):
 
     class ModifyBlockImpl(modify_block):
         def __init__(self):
-            super().__init__()
+            super(ModifyBlockImpl, self).__init__()
 
         def call(self, mat):
             n, m = mat.shape
@@ -61,6 +61,14 @@ def test(mat):
     Jref[:2, :3] = np.arange(6).reshape(2, 3)
 
     assert np.array_equal(Jref, modify.J)
+
+    hasref = has_ref_member()
+    A = np.ones((3, 3)) / 2
+    hasref.Jref[:, :] = A
+    J_true = np.zeros((4, 4))
+    J_true[:3, 1:] = A
+
+    assert np.array_equal(hasref.J, J_true)
 
 
 rows = 10
